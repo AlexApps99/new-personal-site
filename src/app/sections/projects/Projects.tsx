99591de2d4fa1b51928651b2a280aff948358d78
AlexApps99 <alex.apps99@gmail.com>
2024-08-10 18:38:45 +1200

    Adjust styling of project list a little bit

--- src/app/sections/projects/Projects.tsx
+++ src/app/sections/projects/Projects.tsx
@@ -183,7 +183,7 @@ export default function Projects(): Section {
         <ul className="">
           {PROJECTS.map((project) => (
             <li key={project.title} className="">
-              <figure className="m-2 p-2 border-slate-700 bg-slate-600 bg-opacity-20 backdrop-blur-sm border rounded shadow-inner shadow-slate-800 block">
+              <figure className="m-4 p-2 border-slate-700 bg-slate-600 bg-opacity-20 backdrop-blur-sm border rounded shadow-inner shadow-slate-800 block">
                 <figcaption className="font-bold text-xl flex justify-between">
                   <span className="block">
                   {
@@ -201,7 +201,7 @@ export default function Projects(): Section {
                   }</span>
                 </figcaption>
                 {/* <img src={project.image} alt={project.title} className="w-full rounded-t-xl" /> */}
-                <p>
+                <p className="text-slate-400 p-2">
                   {project.description || WALTER_WHITE}
                 </p>
                 <ul className="flex flex-wrap">
